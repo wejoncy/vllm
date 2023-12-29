d68be8beab246557bee7cc4b50a9558a8bc35b43
wejoncy <wejoncy@163.com>
2023-12-29 22:23:00 +0800

    allgather support

--- vllm/model_executor/parallel_utils/communication_op.py
+++ vllm/model_executor/parallel_utils/communication_op.py
@@ -21,6 +21,31 @@ class AllReduce(torch.autograd.Function):
     def symbolic(g: torch.Graph, x) -> torch.Value:
         return g.op('com.microsoft::AllReduce', x)
 
+
+class AllGather(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input_, dim) -> torch.Tensor:
+        input_size = input_.size()
+        world_size = get_tensor_model_parallel_world_size()
+
+        # Allocate output tensor.
+        output_tensor = torch.empty((world_size, ) + input_size,
+                                    dtype=input_.dtype,
+                                    device=input_.device)
+        if torch.onnx.is_in_onnx_export():
+            return output_tensor
+        # All-gather.
+        torch.distributed.all_gather_into_tensor(
+            output_tensor, input_, group=get_tensor_model_parallel_group())
+        return output_tensor
+
+    @staticmethod
+    def symbolic(g: torch.Graph, x, dim) -> torch.Value:
+        world_size = get_tensor_model_parallel_world_size()
+        axes = g.op("Constant", value_t=torch.tensor([0], dtype=torch.int64))
+        x = g.op('Unsqueeze', x, axes)
+        return g.op('com.microsoft::AllGather', x, group_size_i=world_size, axis_i=0)
+
 def tensor_model_parallel_all_reduce(input_):
     """All-reduce the input tensor across model parallel group.
 
@@ -44,13 +69,8 @@ def tensor_model_parallel_all_gather(input_, dim=-1):
         # Convert negative dim to positive.
         dim += input_.dim()
     input_size = input_.size()
-    # Allocate output tensor.
-    output_tensor = torch.empty((world_size, ) + input_size,
-                                dtype=input_.dtype,
-                                device=input_.device)
-    # All-gather.
-    torch.distributed.all_gather_into_tensor(
-        output_tensor, input_, group=get_tensor_model_parallel_group())
+    output_tensor = AllGather.apply(input_, dim)
+
     # Reshape
     output_tensor = output_tensor.movedim(0, dim)
     output_tensor = output_tensor.reshape(input_size[:dim] +
